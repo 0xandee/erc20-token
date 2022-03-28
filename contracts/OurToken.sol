ae71d12339065cac226b78b64b4f0eb35269cee1
Andy Vo <voandy57@gmail.com>
2022-03-28 17:32:17 +0700

    quick erc20 token create

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -4,10 +4,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract GLDToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) public ERC20Detailed("Gold", "GLD", 18) {
+contract OurToken is ERC20 {
+    // wei
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initialSupply);
     }
 }
